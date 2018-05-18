3fddb20fc79e4bed842711ee700ff422ddbc2b1b
= <dingangang@vip.qq.com>
2018-05-18 11:37:35 +0800

    v1

---
+++ src/pages/PageDemo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click="handleClick">Click</button>
+    <child @pfn="handlePfn"></child>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
+</template>
+
+<script scoped>
+import Child from '@/components/Child';
+
+export default{
+  props: [],
+  methods: {
+    handleClick: function () {
+      console.log(1);
+    },
+    handlePfn: function () {
+      console.log('from child');
+    },
+  },
+  components: {
+    Child,
+  },
+};
+</script>
+
+<style scoped></style>
