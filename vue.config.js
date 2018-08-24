a6e47bbe3cfc34e4c393db58106f3403efedadfe
= <dingangang@vip.qq.com>
2018-08-24 16:29:22 +0800

    优化dist后结构

--- vue.config.js
+++ vue.config.js
@@ -22,7 +22,7 @@ module.exports = {
       new SpritesPlugin({
         // 目标小图标
         src: {
-          cwd: path.resolve(__dirname, './public/images/icons'),
+          cwd: path.resolve(__dirname, './public/static/images/icons'),
           glob: '*.png'
         },
         // 输出雪碧图文件及样式文件
@@ -35,7 +35,7 @@ module.exports = {
           ]
         },
         customTemplates: {
-          function_based_template: path.resolve(__dirname, './public/sprite_handlebars_template.handlebars')
+          function_based_template: path.resolve(__dirname, './public/static/sprite_handlebars_template.handlebars')
         },
         // 样式文件中调用雪碧图地址写法
         apiOptions: {
